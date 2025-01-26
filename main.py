bfae4c7583a1602abb8e9effa97e456b5ff7e8b1
PitRc47 <189371188+PitRc47@users.noreply.github.com>
2025-01-26 21:24:44 +0800

    test: fix webview

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import webview
 
 if __name__ == '__main__':
     # 设置允许文件下载（如果需要）
-    webview.settings['ALLOW_DOWNLOADS'] = True
-
     # 创建并显示一个 WebView 窗口
     webview.create_window('My PyWebView App', 'https://www.example.com')
     webview.start()
