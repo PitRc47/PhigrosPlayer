3158789f2dfee3ff66839e4aae97bdd1c06bdac1
PitRc47 <189371188+PitRc47@users.noreply.github.com>
2025-01-26 21:37:37 +0800

    fix: main.py webview

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import webview
 
-if __name__ == '__main__':
-    # 设置允许文件下载（如果需要）
-    # 创建并显示一个 WebView 窗口
-    webview.create_window('My PyWebView App', 'https://www.example.com')
-    webview.start()
+webview.create_window('My PyWebView App', 'https://www.example.com')
+webview.start()
