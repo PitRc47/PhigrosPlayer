a290763f9319bca090ae5903f0081e48eca8af65
PitRc47 <189371188+PitRc47@users.noreply.github.com>
2025-01-27 13:56:15 +0800

    fix: src path

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
+import os
+import socket
+import traceback
+
 def start_client(server_ip='192.168.1.28', server_port=7878):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, server_port))
+    client_socket.connect_ex((server_ip, server_port))
     return client_socket
 def main():
     root = webcv.WebCanvas(
@@ -18,13 +23,21 @@ def main():
     while True:
         root.run_js_code("console.log('Hello, World!');")
 
-import socket
-import traceback
 client_socket = start_client()
 try:
     import webview
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    src_dir = os.path.join(current_dir, 'src')
+    sys.path.append(src_dir)
+
     import webcv
     main()
+    
 except Exception as e:
     error_message = f"Error occurred: {traceback.format_exc()}"
-    client_socket.send(error_message.encode('utf-8'))
+    print(error_message)
+    try:
+        client_socket.send(error_message.encode('utf-8'))
+    except:
+        pass
