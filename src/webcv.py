4f693cf5dd8c5d14b7c82bed796d1757fe958ec5
PitRc47 <189371188+PitRc47@users.noreply.github.com>
2025-02-03 20:02:42 +0800

    Update webcv.py

--- src/webcv.py
+++ src/webcv.py
@@ -329,7 +329,11 @@ class WebCanvas:
                 try:
                     self.web.native.webview.setWebContentsDebuggingEnabled(True)
                     logging.info('Webview getSettings')
-                    
+                    logging.info(f'self.web.native.webview.getSettings(): {self.web.native.webview.getSettings()}')
+                    self.web.native.webview.getSettings().setAllowUniversalAccessFromFileURLs(True)
+                    self.web.native.webview.getSettings().setAllowFileAccessFromFileURLs(True)
+                    self.web.native.webview.getSettings().setAllowContentAccess(True)
+                    self.web.native.webview.getSettings().setMixedContentMode(1)
                 except:
                     continue
                 break
