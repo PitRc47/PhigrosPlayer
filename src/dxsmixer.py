0ee9b19bc2b6d929fa8fe125c5a78af7554cad74
PitRc47 <189371188+PitRc47@users.noreply.github.com>
2025-02-05 08:55:54 +0800

    Revert "Update dxsmixer.py"
    This reverts commit 4bc6461f8012dd1232671b481afe813f6046004a.

--- src/dxsmixer.py
+++ src/dxsmixer.py
@@ -65,23 +65,17 @@ class musicCls:
     def play(self, isloop: typing.Literal[0, -1] = 0):
         self.lflag = 0 if isloop == 0 else 1
         
-        try:
-            if self.buffer is None:
-                _, self.buffer = self.dxs.create(self.lflag)
-                self._setBufferVolume(self._volume)
-                if checksys.main == 'Android':
-                    self.buffer.prepare()
-            else:
-                self.set_pos(0.0)
-                if checksys.main == 'Android':
-                    if not self.buffer.isPlaying():
-                        self.buffer.start()
-                else:
-                    self.buffer.Play(self.lflag)
-        except Exception as e:
-            print(f"Play error: {e}")
-            self.buffer = None
+        if self.buffer is None:
+            _, self.buffer = self.dxs.create(self.lflag)
+            self._setBufferVolume(self._volume)
             
+        else:
+            self.set_pos(0.0)
+            if checksys.main == 'Android':
+                self.buffer.start()
+            else:
+                self.buffer.Play(self.lflag)
+        
     def stop(self):
         if checksys.main == 'Android' and self.buffer is not None:
             self.buffer.release()
